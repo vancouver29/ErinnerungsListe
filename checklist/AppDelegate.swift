f91afc4b5153102900aea7323114335af1456310
vancouver29 <vancouver2990@gmail.com>
2020-03-26 13:59:41 +0100

    handling local notification events

--- checklist/AppDelegate.swift
+++ checklist/AppDelegate.swift
@@ -10,31 +10,32 @@ import UIKit
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Notification authorization
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) {
-          granted, error in
-          if granted {
-            print("We have permission")
-          } else {
-            print("Permission denied")
-          }
-        }
-        
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "I am a local notification"
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10,repeats: false)
-        let request = UNNotificationRequest(identifier: "MyNotification",
-                                            content: content,
-                                            trigger: trigger)
-        center.add(request)
+//        center.requestAuthorization(options: [.alert, .sound]) {
+//          granted, error in
+//          if granted {
+//            print("We have permission")
+//          } else {
+//            print("Permission denied")
+//          }
+//        }
+//
+//        let content = UNMutableNotificationContent()
+//        content.title = "Hello!"
+//        content.body = "I am a local notification"
+//        content.sound = UNNotificationSound.default
+//
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10,repeats: false)
+//        let request = UNNotificationRequest(identifier: "MyNotification",
+//                                            content: content,
+//                                            trigger: trigger)
+//        center.add(request)
+        center.delegate = self
         return true
     }
 
@@ -50,6 +51,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK:- User Notification Delegates
+    func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification, withCompletionHandler completionHandler:
+    @escaping (UNNotificationPresentationOptions) -> Void) {
+      print("Received local notification \(notification)")
     }
 }
 
